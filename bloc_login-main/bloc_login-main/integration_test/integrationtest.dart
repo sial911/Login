44def24c83d5f9bdfc409ae94a8cdf5c34c80e5d
sial911 <hsnwsm911@gmail.com>
2022-03-20 12:06:59 +0500

    Login

--- bloc_login-main/bloc_login-main/integration_test/integrationtest.dart
+++ bloc_login-main/bloc_login-main/integration_test/integrationtest.dart
@@ -28,10 +28,8 @@ void main() {
 
       await tester.pumpAndSettle(const Duration(seconds: 3));
 
-// Finds the floating action button to tap on.
       final Finder lg = find.widgetWithText(ElevatedButton, "Login");
 
-// Emulate a tap on the floating action button.
       await tester.tap(lg);
 
       await tester.pumpAndSettle();
@@ -75,53 +73,14 @@ void main() {
       await tester.pumpAndSettle();
 
       await tester.pumpAndSettle(const Duration(seconds: 3));
-
-// Verify the counter increments by 1.
-//         expect(find.text('Login '), findsOneWidget);
-
-// await tester.enterText(find.text("Username or Email"), 'ali@gmail.com');
       await tester.enterText(find.byKey(const Key('login_email')), 'qwe');
       await tester.enterText(find.byKey(const Key('login_pwd')), '1234');
 
       await tester.pumpAndSettle(const Duration(seconds: 3));
-// await tester.enterText(find.byType(TextField), '123456');
 
       final Finder login = find.widgetWithText(ElevatedButton, "Login");
 
       await tester.tap(login);
-
-      // await Future.delayed(const Duration(seconds: 5), () {});
-
-      // await tester.pumpAndSettle(Duration(seconds: 4));
-// await Future.delayed(const Duration(seconds: 10), (){});
-//
-//         final Finder drawer = find.byIcon(Icons.menu);
-//
-//         await tester.tap(drawer);
-// // Trigger a frame.
-//         await tester.pumpAndSettle();
-// // await Future.delayed(const Duration(seconds: 5), (){});
-//         final Finder currencyconverter = find.text("Currency Converter");
-//
-// // Emulate a tap on the floating action button.
-//         await tester.tap(currencyconverter);
-//
-// // Trigger a frame.
-//         await tester.pumpAndSettle();
-// // await Future.delayed(const Duration(seconds: 5), (){});
-//
-//
-//         await tester.enterText(find.byKey(ValueKey('amount')), '100');
-//         await tester.pumpAndSettle();
-// // await Future.delayed(const Duration(seconds: 5), (){});
-//         await Future.delayed(const Duration(seconds: 5), () {});
-//         await tester.pumpAndSettle();
-//         final Finder convert = find.widgetWithText(ElevatedButton, "Convert");
-//         await tester.pumpAndSettle();
-//         await tester.tap(convert);
-//         await tester.pumpAndSettle();
-//
-//         await Future.delayed(const Duration(seconds: 5), () {});
     });
   });
 }
